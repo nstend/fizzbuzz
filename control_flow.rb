fb4a0dc7275cb32f3d99fe3625a935710c8ba817
Nina Stender <nstender@wesleyan.edu>
2014-02-18 13:44:19 -0500

    control flow for fizzbuzz

---
+++ control_flow.rb
@@ -0,0 +1,11 @@
+#get number for fizzbuzz
+puts "gimme a number yo"
+n = gets.to_i
+
+if (n % 15.0 == 0)
+	puts "fizzbuzz"
+elsif (n % 3.0) == 0 
+	puts "fizz"
+elsif (n % 5.0) == 0 
+	puts "buzz"
+end
